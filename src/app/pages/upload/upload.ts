10f5fbc0362f8b967f4337c5bb436d3d02c08a1b
Sarah Motta <sarahmotta@Mac-mini-de-Sarah.local>
2026-04-15 16:42:22 -0300

    feat: adiciona componente de upload de guia

---
+++ src/app/pages/upload/upload.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-upload',
+  standalone: true,
+  imports: [],
+  templateUrl: './upload.html',
+  styleUrl: './upload.css'
+})
+export class UploadComponent {
+
+}

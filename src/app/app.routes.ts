5d26949664313fc45e6f545465e9851157fbcbb4
Nicolas Catelan <nicolascatelan9@gmail.com>
2026-05-25 18:59:50 -0300

    feat: Adicionado tela da guia

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { UploadComponent } from './pages/upload/upload';
+import { Guia } from './pages/guia/guia';
 
 export const routes: Routes = [
   {
     path: '',
     component: UploadComponent,
+  },
+  {
+    path: 'guia',
+    component: Guia,
   },
 ];
